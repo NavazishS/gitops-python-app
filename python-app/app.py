f98b091fb07ab3233c32c4b28c865fbe91122036
Navazish <72895399+NavazishS@users.noreply.github.com>
2023-08-16 14:51:12 +0200

    Update app.py

--- python-app/app.py
+++ python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_answer():
-    return 'This is from Python app :42'
+    return 'This is from Python app :42 \n'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
